e9a826dca297f0b32102ae27573e7c94bc792d7b
Gabriel Pereira <ggonpereira@gmail.com>
2022-10-12 17:34:52 -0300

    💎 style: fix wrong style on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./styles/global.css";
 
 const App = () => {
-  return <h1 className="font-bold text-5xl text-violet-500">Hello World</h1>;
+  return <h1 className="font-bold text-2xl text-cyan-500">Hello World</h1>;
 };
 
 export default App;
